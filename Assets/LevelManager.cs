5a8f829282eb947b20f8e128322bc48a6ad62da9
Justin Peterson <sleepy@Justins-MacBook-Pro.local>
2017-11-01 08:37:04 -0700

    Added level manager, start and quite buttons

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Level load requested for " + name);
+	}
+	
+	public void Quit_Request(){
+		Debug.Log ("Quit Requested");
+	}
+}
---
+++ Assets/QuitManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitManager : MonoBehaviour {
+
+	public void Quit_Request(){
+		Debug.Log ("Quit Requested");
+	}
+}
