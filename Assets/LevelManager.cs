c925f33cb0bbf235642ef02bb3650c6d4cf77c94
Justin Peterson <sleepy@Justins-MacBook-Pro.local>
2017-11-01 09:03:34 -0700

    Added scene transition

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name){
-		Debug.Log ("Level load requested for " + name);
+		Debug.Log ("Loading level " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void BackToStart(){		
+		Application.LoadLevel("Start");
 	}
 	
 	public void Quit_Request(){
-		Debug.Log ("Quit Requested");
+		Application.Quit();
 	}
 }
--- Assets/QuitManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class QuitManager : MonoBehaviour {
-
-	public void Quit_Request(){
-		Debug.Log ("Quit Requested");
-	}
-}
