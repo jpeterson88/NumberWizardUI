99eda3fc0fc6e63c1fa41bd1ef894dd88e036716
Justin Peterson <sleepy@Justins-MacBook-Pro.local>
2017-11-05 16:33:52 -0800

    Finished number wizard UI

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	
+	private int max, min, guess, maxGuessesAllowed;
+	
+	public Text text;
+	
+	public void Start () {
+		max = 1001;
+		min = 1;
+		maxGuessesAllowed = 5;	
+		guess = 500;
+	}	
+
+	public void GuessHigher(){
+		min = guess;
+		nextGuess();
+	}
+	
+	public void GuessLower(){
+		max = guess;
+		nextGuess();
+	}	
+	
+	private void nextGuess(){		
+		guess = Random.Range(min, max);
+		
+		maxGuessesAllowed -= 1;
+		
+		text.text = guess.ToString();
+		
+		 if(maxGuessesAllowed <= 0){
+		 	Application.LoadLevel("Lose");
+		 } 
+	}
+}
